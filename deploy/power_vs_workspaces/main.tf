75fcd443589e5b8da3db35e7c817c0521be389c7
Samuel Matzek <smatzek@us.ibm.com>
2024-02-21 15:21:45 -0600

    Change provider for workspaces

--- deploy/power_vs_workspaces/main.tf
+++ deploy/power_vs_workspaces/main.tf
@@ -9,6 +9,7 @@ provider "ibm" {
   ibmcloud_timeout = 60
 }
 
+/*
 provider "ibm" {
   alias            = "power_vs_wdc07"
   ibmcloud_api_key = var.ibmcloud_api_key
@@ -151,6 +152,7 @@ provider "ibm" {
   zone             = "syd04"
   ibmcloud_timeout = 60
 }
+*/
 
 ##############################################################################
 
@@ -180,7 +182,7 @@ locals {
 ##############################################################################
 
 resource "ibm_resource_instance" "power_vs_workspace_craig_wdc07" {
-  provider          = ibm.power_vs_wdc07
+  #provider          = ibm.power_vs_wdc07
   name              = "${var.prefix}-power-workspace-craig-wdc07"
   service           = "power-iaas"
   plan              = "power-virtual-server-group"
@@ -201,7 +203,7 @@ resource "ibm_resource_instance" "power_vs_workspace_craig_wdc07" {
 ##############################################################################
 
 resource "ibm_resource_instance" "power_vs_workspace_craig_tor01" {
-  provider          = ibm.power_vs_tor01
+  #provider          = ibm.power_vs_tor01
   name              = "${var.prefix}-power-workspace-craig-tor01"
   service           = "power-iaas"
   plan              = "power-virtual-server-group"
@@ -222,7 +224,7 @@ resource "ibm_resource_instance" "power_vs_workspace_craig_tor01" {
 ##############################################################################
 
 resource "ibm_resource_instance" "power_vs_workspace_craig_sao01" {
-  provider          = ibm.power_vs_sao01
+  #provider          = ibm.power_vs_sao01
   name              = "${var.prefix}-power-workspace-craig-sao01"
   service           = "power-iaas"
   plan              = "power-virtual-server-group"
@@ -243,7 +245,7 @@ resource "ibm_resource_instance" "power_vs_workspace_craig_sao01" {
 ##############################################################################
 
 resource "ibm_resource_instance" "power_vs_workspace_craig_sao04" {
-  provider          = ibm.power_vs_sao04
+  #provider          = ibm.power_vs_sao04
   name              = "${var.prefix}-power-workspace-craig-sao04"
   service           = "power-iaas"
   plan              = "power-virtual-server-group"
@@ -264,7 +266,7 @@ resource "ibm_resource_instance" "power_vs_workspace_craig_sao04" {
 ##############################################################################
 
 resource "ibm_resource_instance" "power_vs_workspace_craig_tok04" {
-  provider          = ibm.power_vs_tok04
+  #provider          = ibm.power_vs_tok04
   name              = "${var.prefix}-power-workspace-craig-tok04"
   service           = "power-iaas"
   plan              = "power-virtual-server-group"
@@ -285,7 +287,7 @@ resource "ibm_resource_instance" "power_vs_workspace_craig_tok04" {
 ##############################################################################
 
 resource "ibm_resource_instance" "power_vs_workspace_craig_dal12" {
-  provider          = ibm.power_vs_dal12
+  #provider          = ibm.power_vs_dal12
   name              = "${var.prefix}-power-workspace-craig-dal12"
   service           = "power-iaas"
   plan              = "power-virtual-server-group"
@@ -306,7 +308,7 @@ resource "ibm_resource_instance" "power_vs_workspace_craig_dal12" {
 ##############################################################################
 
 resource "ibm_resource_instance" "power_vs_workspace_craig_dal10" {
-  provider          = ibm.power_vs_dal10
+  #provider          = ibm.power_vs_dal10
   name              = "${var.prefix}-power-workspace-craig-dal10"
   service           = "power-iaas"
   plan              = "power-virtual-server-group"
@@ -327,7 +329,7 @@ resource "ibm_resource_instance" "power_vs_workspace_craig_dal10" {
 ##############################################################################
 
 resource "ibm_resource_instance" "power_vs_workspace_craig_us_south" {
-  provider          = ibm.power_vs_us_south
+  #provider          = ibm.power_vs_us_south
   name              = "${var.prefix}-power-workspace-craig-us-south"
   service           = "power-iaas"
   plan              = "power-virtual-server-group"
@@ -348,7 +350,7 @@ resource "ibm_resource_instance" "power_vs_workspace_craig_us_south" {
 ##############################################################################
 
 resource "ibm_resource_instance" "power_vs_workspace_craig_wdc06" {
-  provider          = ibm.power_vs_wdc06
+  #provider          = ibm.power_vs_wdc06
   name              = "${var.prefix}-power-workspace-craig-wdc06"
   service           = "power-iaas"
   plan              = "power-virtual-server-group"
@@ -369,7 +371,7 @@ resource "ibm_resource_instance" "power_vs_workspace_craig_wdc06" {
 ##############################################################################
 
 resource "ibm_resource_instance" "power_vs_workspace_craig_us_east" {
-  provider          = ibm.power_vs_us_east
+  #provider          = ibm.power_vs_us_east
   name              = "${var.prefix}-power-workspace-craig-us-east"
   service           = "power-iaas"
   plan              = "power-virtual-server-group"
@@ -390,7 +392,7 @@ resource "ibm_resource_instance" "power_vs_workspace_craig_us_east" {
 ##############################################################################
 
 resource "ibm_resource_instance" "power_vs_workspace_craig_lon06" {
-  provider          = ibm.power_vs_lon06
+  #provider          = ibm.power_vs_lon06
   name              = "${var.prefix}-power-workspace-craig-lon06"
   service           = "power-iaas"
   plan              = "power-virtual-server-group"
@@ -411,7 +413,7 @@ resource "ibm_resource_instance" "power_vs_workspace_craig_lon06" {
 ##############################################################################
 
 resource "ibm_resource_instance" "power_vs_workspace_craig_lon04" {
-  provider          = ibm.power_vs_lon04
+  #provider          = ibm.power_vs_lon04
   name              = "${var.prefix}-power-workspace-craig-lon04"
   service           = "power-iaas"
   plan              = "power-virtual-server-group"
@@ -432,7 +434,7 @@ resource "ibm_resource_instance" "power_vs_workspace_craig_lon04" {
 ##############################################################################
 
 resource "ibm_resource_instance" "power_vs_workspace_craig_eu_de_2" {
-  provider          = ibm.power_vs_eu_de_2
+  #provider          = ibm.power_vs_eu_de_2
   name              = "${var.prefix}-power-workspace-craig-eu-de-2"
   service           = "power-iaas"
   plan              = "power-virtual-server-group"
@@ -453,7 +455,7 @@ resource "ibm_resource_instance" "power_vs_workspace_craig_eu_de_2" {
 ##############################################################################
 
 resource "ibm_resource_instance" "power_vs_workspace_craig_eu_de_1" {
-  provider          = ibm.power_vs_eu_de_1
+  #provider          = ibm.power_vs_eu_de_1
   name              = "${var.prefix}-power-workspace-craig-eu-de-1"
   service           = "power-iaas"
   plan              = "power-virtual-server-group"
@@ -474,7 +476,7 @@ resource "ibm_resource_instance" "power_vs_workspace_craig_eu_de_1" {
 ##############################################################################
 
 resource "ibm_resource_instance" "power_vs_workspace_craig_mad02" {
-  provider          = ibm.power_vs_mad02
+  #provider          = ibm.power_vs_mad02
   name              = "${var.prefix}-power-workspace-craig-mad02"
   service           = "power-iaas"
   plan              = "power-virtual-server-group"
@@ -495,7 +497,7 @@ resource "ibm_resource_instance" "power_vs_workspace_craig_mad02" {
 ##############################################################################
 
 resource "ibm_resource_instance" "power_vs_workspace_craig_mad04" {
-  provider          = ibm.power_vs_mad04
+  #provider          = ibm.power_vs_mad04
   name              = "${var.prefix}-power-workspace-craig-mad04"
   service           = "power-iaas"
   plan              = "power-virtual-server-group"
@@ -516,7 +518,7 @@ resource "ibm_resource_instance" "power_vs_workspace_craig_mad04" {
 ##############################################################################
 
 resource "ibm_resource_instance" "power_vs_workspace_craig_syd05" {
-  provider          = ibm.power_vs_syd05
+  #provider          = ibm.power_vs_syd05
   name              = "${var.prefix}-power-workspace-craig-syd05"
   service           = "power-iaas"
   plan              = "power-virtual-server-group"
@@ -537,7 +539,7 @@ resource "ibm_resource_instance" "power_vs_workspace_craig_syd05" {
 ##############################################################################
 
 resource "ibm_resource_instance" "power_vs_workspace_craig_syd04" {
-  provider          = ibm.power_vs_syd04
+  #provider          = ibm.power_vs_syd04
   name              = "${var.prefix}-power-workspace-craig-syd04"
   service           = "power-iaas"
   plan              = "power-virtual-server-group"
